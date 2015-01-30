90c74beedb326798773fa8a3b38dd031ac0e8bb9
Trey Phillips <tlphillipsjr@gmail.com>
2015-01-29 22:14:16 -0500

    sucessfully talking to/recieving from server

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,37 @@
-(function () {
-
-  "use strict";
-
-  var baseURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+(function() {
+  'use strict';
 
 
+  $(document).ready(function() {
+
+      var baseURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+      
+
+      $.ajax({
+        url: baseURL,
+        type: "GET"
+      }).done(function(data) {
+        console.log(data);
+
+      });
+
+      $.ajax({
+      url: baseURL,
+      type: "POST",
+      data: {
+        message: "lkjdlkjdljdl",
+        username: "Gringo Joe",
+        createdAt: Date.now()
+      }
+    }).done(function(data) {
+      console.log(data);
+
+    });
+
+      $.getJSON(baseURL).done(function(items) {
+        renderTemplate('message-reply-template', 'messageboard-container', result);
+      })
 
 
-})();
+    });
+  })();
