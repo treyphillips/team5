362f953abf7f7771bc5e586b5f0408e38ef061fc
Daniel Edwards <cdaniel.edwards@gmail.com>
2015-01-30 15:42:33 -0500

    commit with working log need to fix username

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,57 @@
   'use strict';
 
   var baseURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+  var username = [];
 
   $(document).ready(function() {
 
 
 
-
+      var baseURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
       // var userName = [];
       // userName.push($("#input-userName").submit().value());
       // console.log(userName);
 
       var $messageboardContainer = $('.messageboard-container');
+
       var messageboardTemplate = _.template($('[data-template-name=message-post]').text());
       $.ajax(baseURL).done(function(posts) {
         _.each(posts, function(post){
           _.defaults(post, {
-            message: "",
-            username: "",
+            message: "No Message",
+            username: "Anonymous",
             createdAt: ""
           });
           $messageboardContainer.append(messageboardTemplate(post));
         });
       });
 
-      
+      $('.submit').click(function() {
+        username = (".username").value();
+        console.log('username');
+      });
+
+      // $.ajax({
+      //   url: baseURL,
+      //   type: "GET"
+      //
+      // }).done(function(posts) {
+      //   _.each(posts, function(post){
+      //     _.defaults(post, {
+      //       message: "",
+      //       username: "",
+      //       createdAt: ""
+      //     });
+      //     $messageboardContainer.append(messageboardTemplate(post));
+      //   });
+      //
+      // });
+
+
+
+      // $.getJSON(baseURL).done(function(items) {
+      //   renderTemplate('message-reply-template', 'messageboard-container', result);
+      // })
 
 
     });
